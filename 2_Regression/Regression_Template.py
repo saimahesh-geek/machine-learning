e88f0dc8c903ee8436a318b29239eeed62e852f5
Sai Maheshwara Reddy Manda <saimahesh.cse@gmail.com>
2018-11-15 16:17:30 +0530

    second commit

--- 2_Regression/Regression_Template.py
+++ 2_Regression/Regression_Template.py
@@ -13,6 +13,12 @@ y = dataset.iloc[:, -1].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
+#Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+
 #Fitting the Regression to the dataset
 #Create your regressor here
 
