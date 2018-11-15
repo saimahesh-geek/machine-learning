227e28a36fe668c611a3f3f1ae0548a683d385ae
Sai Maheshwara Reddy Manda <saimahesh.cse@gmail.com>
2018-11-15 23:45:20 +0530

    second commit

--- 3_Classification/SVM.R
+++ 3_Classification/SVM.R
@@ -15,17 +15,24 @@ test_set = subset(dataset, split == FALSE)
 training_set[, 1:2] = scale(training_set[, 1:2]) 
 test_set[, 1:2] = scale(test_set[, 1:2])
 
-#Fitting the classifier to dataset - glm -> Generalized Linear Model
-#Create your classifier here
+#Fitting the SVM classifier to dataset
+library(e1071)
+classifier = svm(formula = Purchased ~ .,
+                 data = training_set,
+                 type = 'C-classification',
+                 kernel = 'linear')
 
 
 #Predicting the test set results
-# prob_pred = predict(classifier, type = 'response', newdata = test_set[-3])
-# y_pred = ifelse(prob_pred > 0.5, 1, 0)
 y_pred = predict(classifier, newdata = test_set[-3])
 
 #Making the confusion matrix
 cm = table(test_set[, 3], y_pred)
+
+# y_pred
+#    0  1
+# 0 57  7
+# 1 13 23
 
 #Visualising the the training set
 #install.packages("ElemStatLearn")
@@ -37,7 +44,7 @@ grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
 y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'Classifier (Training set)',
+     main = 'SVM Classifier (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -53,7 +60,7 @@ grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
 y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'Classifier (Test set)',
+     main = 'SVM Classifier (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
